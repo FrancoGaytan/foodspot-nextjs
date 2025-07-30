eab164103f5e671e2c3c667a2311978b126f16f1
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-30 10:57:57 -0300

    image slider added

--- src/components/pages/EventHome/index.tsx
+++ src/components/pages/EventHome/index.tsx
@@ -2,6 +2,8 @@ import styles from './styles.module.scss';
 import EventsContainer from './EventsContainer';
 import HomeHeader from './HomeHeader';
 import { getTranslation } from '@utils/getTranslation';
+import { eventImages } from '@utils/eventImages';
+import ImageSlider from '@components/macro/Slider';
 
 export default async function eventHome() {
   const { t } = await getTranslation('eventHome');
@@ -11,6 +13,9 @@ export default async function eventHome() {
       <h1 className={styles.incomingEventTitle}>{t.incomingEvents}</h1>
       <section className={styles.eventsContainer}>
         <EventsContainer />
+      </section>
+      <section className={styles.carruselContainer}>
+        <ImageSlider images={eventImages} altText="Event image" />
       </section>
     </div>
   );
